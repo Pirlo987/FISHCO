5ca678a3de393d1996eea46eb25b420e3a1088e0
Brieuc Vannier <brieuc.vannier931@gmail.com>
2026-02-04 21:31:56 -1000

    species page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -36,7 +36,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explorer',
+          title: 'Fishdex',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="chevron.right" color={color} />,
         }}
       />
